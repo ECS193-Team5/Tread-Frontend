1edde2ee4a6ba11bd363bf46962f1639e0bae554
Rebekah Grace <rmgrace@ucdavis.edu>
2023-05-11 21:25:36 -0700

    pages test routes added

--- tests/helperFunction.js
+++ tests/helperFunction.js
@@ -1,16 +1,22 @@
-
+// @ts-check
+const { test, expect } = require('@playwright/test');
 export async function loginTread1(page) {
-    await page.goto('https://tread.run/');
-    const page1Promise = page.waitForEvent('popup');
-    await page.frameLocator('iframe[title="Sign in with Google Button"]').getByRole('button', { name: 'Sign in with Google' }).click();
-    const page1 = await page1Promise;
+    const [page1] = await Promise.all([
+        page.waitForEvent('popup'),
+        page.getByRole('button', { name: 'Sign in with Google' }).click()
+    ])
+    console.log(await page1.title());
     await page1.getByRole('textbox', { name: 'Email or phone' }).click();
     await page1.getByRole('textbox', { name: 'Email or phone' }).fill('treadtestuser1@gmail.com');
     await page1.getByRole('button', { name: 'Next' }).click();
     await page1.getByRole('textbox', { name: 'Enter your password' }).click();
+    await page1.getByRole('textbox', { name: 'Enter your password' }).fill('Tr');
+    await page1.locator('div').filter({ hasText: /^Enter your passwordShow passwordType the text you hear or see$/ }).first().click();
+    await page1.getByRole('textbox', { name: 'Enter your password' }).click();
     await page1.getByRole('textbox', { name: 'Enter your password' }).fill('TreadTest!!!12');
     await page1.getByRole('button', { name: 'Next' }).click();
     await page.goto('https://tread.run/currentChallengePage');
+    await expect(page).toHaveURL('https://tread.run/currentChallengePage');
 }
 
 export async function loginTread2(page) {
--- tests/unit/Challenge/acceptChallengeButton.spec.js
+++ tests/unit/Challenge/acceptChallengeButton.spec.js
@@ -2,34 +2,10 @@
 import { loginTread1, loginTread2 } from '../../helperFunction';
 const { test, expect } = require('@playwright/test');
 
-test('loginworks', async ({ page }) => {
+test('login works', async ({ page }) => {
     await page.goto('https://tread.run/');
-    const page1Promise = page.waitForEvent('popup');
-    const [page1] = await Promise.all([
-        page.waitForEvent('popup'),
-        page.getByRole('button', { name: 'Sign in with Google' }).click()
-    ])
-    console.log(await page1.title());
-    await page1.getByRole('textbox', { name: 'Email or phone' }).click();
-    await page1.getByRole('textbox', { name: 'Email or phone' }).fill('treadtestuser1@gmail.com');
-    await page1.getByRole('button', { name: 'Next' }).click();
-    await page1.getByRole('textbox', { name: 'Enter your password' }).click();
-    await page1.getByRole('textbox', { name: 'Enter your password' }).fill('Tr');
-    await page1.locator('div').filter({ hasText: /^Enter your passwordShow passwordType the text you hear or see$/ }).first().click();
-    await page1.getByRole('textbox', { name: 'Enter your password' }).click();
-    await page1.getByRole('textbox', { name: 'Enter your password' }).fill('TreadTest!!!12');
-    await page1.getByRole('button', { name: 'Next' }).click();
-    await page.goto('https://tread.run/currentChallengePage');
-    /*
-    await page1.getByRole('textbox', { name: 'Email or phone' }).click();
-    await page1.getByRole('textbox', { name: 'Email or phone' }).fill('treadtestuser1@gmail.com');
-    await page1.getByRole('button', { name: 'Next' }).click();
-    await page1.getByRole('textbox', { name: 'Enter your password' }).click();
-    await page1.getByRole('textbox', { name: 'Enter your password' }).fill('TreadTest!!!12');
-    await page1.getByRole('button', { name: 'Next' }).click();
-    await page.goto('https://tread.run/currentChallengePage');
-    await expect(page).toHaveURL("/currentChallengePage");
-    */
+    await loginTread1(page);
+
 });
 
 /*
@@ -38,6 +14,7 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
+
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
