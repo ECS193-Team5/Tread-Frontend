577771ed384946be64bfff9863da0d160399e0b5
Rebekah Grace <rmgrace@ucdavis.edu>
2023-05-20 23:02:48 -0700

    position left to the user settings dropdown

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,12 +2,14 @@ import PhotoDisplay from "../components/Challenge/PhotoDisplay";
 import ShowDueDate from "../components/Challenge/ShowDueDate";
 import LeagueMemberScroll from "../components/League/LeagueMemberScroll";
 import LeagueMemberSection from "../components/League/LeagueMemberSection";
+import UserSettingsButtton from "../components/Shared/UserSettingsButton";
 const TestDiv = () => {
 
 
 
     return(
         <div>
+            <UserSettingsButtton></UserSettingsButtton>
         </div>
     );
 }
